5cb7f6fa1d7affa3ad0c45550f13c6a778368f47
Tetiana5Buria <poriniya@gmail.com>
2025-07-29 14:49:25 +0300

    checked work

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 /* burger-menu */
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const burger = document.getElementById("burger");
   const navLinks = document.getElementById("navLinks");
 
-  burger.addEventListener("click", function () {
+  burger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   });
 });
