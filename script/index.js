52d4edc9c7fac954f2c2e986aff53a8422100e5c
Tetiana5Buria <poriniya@gmail.com>
2025-07-29 14:12:53 +0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,19 @@
+/* skills */
+window.addEventListener("scroll", function () {
+  const skillsSection = document.getElementById("Skills");
+  const sectionTop = skillsSection.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (sectionTop < windowHeight - 100) {
+    const bars = document.querySelectorAll(".progress_bar");
+    for (let i = 0; i < bars.length; i++) {
+      const span = bars[i].querySelector("span");
+      const percent = bars[i].getAttribute("data-percentage");
+      span.style.width = percent + "%";
+      span.style.background = "linear-gradient(45deg, #272929, #0ef)";
+    }
+
+    // Знімаємо слухач, щоб не запускати знову
+    window.removeEventListener("scroll", arguments.callee);
+  }
+});
