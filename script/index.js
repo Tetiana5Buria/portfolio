4b329b4516083588d42e8e38be86f9fb22eeec97
Tetiana5Buria <poriniya@gmail.com>
2025-12-18 15:02:17 +0200

    added modal cert

--- script/index.js
+++ script/index.js
@@ -94,3 +94,55 @@ document.querySelectorAll(".carousel-wrapper").forEach((wrapper) => {
     carousel.scrollBy({ left: -scrollAmount(), behavior: "smooth" });
   });
 });
+
+/* scale cert */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const closeBtn = document.querySelector(".close");
+  const images = document.querySelectorAll(".achievement-icon img");
+
+  images.forEach((img) => {
+    img.addEventListener("click", () => {
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      modalImg.alt = img.alt;
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
+/* document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const closeBtn = document.querySelector(".close");
+  const images = document.querySelectorAll(".achievement-icon img");
+
+  images.forEach((img) => {
+    img.addEventListener("click", () => {
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      modalImg.alt = img.alt;
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+ */
